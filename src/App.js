578a5eb7a7cb7753a953e1a7584c149f8e1b098f
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-12-22 10:10:29 +0300

    Todo App Structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,36 @@ function App() {
   return (
     <div className="App">
      <h1>Todos</h1>
+     <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className='todo-input-item'> 
+            <label htmlFor="title">Title: </label>
+            <input type="text" placeholder="What's the Task Title?" />
+          </div>
+
+          <div className='todo-input-item'> 
+            <label htmlFor="title">Description: </label>
+            <input type="text" placeholder="What's the Task Description?" />
+          </div>
+          
+          <div className='todo-input-item'> 
+            <button className='primaryBtn' type='button'>Add</button>
+          </div>
+        </div>
+
+        <div className='btn-area'>
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+
+        <div className='todo-list'>
+          <div className='todo-list-item'>
+            <h1>Task 1</h1>
+            <p>This is a description for task one.</p>
+          </div>
+        </div>
+        
+     </div>
     </div>
   );
 }
